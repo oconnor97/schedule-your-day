0f81e1df8e3c5fe2ade8a4a9ee1e121941d8a590
Michael O'Connor <oconnor97@aol.com>
2021-02-22 17:10:57 -0500

    creating a function to compare the current time to the time ids and assigning new classes based on the result

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,17 +3,23 @@ var currentDay = $('#currentDay');
 var currentTime = moment().startOf('hour');
 var beforeTime = moment().startOf('day').add(9, 'hours');
 var time9 = $('#time9');
-
-var time13 = $('#time13');
+var time10 = $('#time10');
+var time11 = $('#time11');
+var time12 = $('#time12');
+var time1 = $('#time1');
+var time2 = $('#time2');
+var time3 = $('#time3');
+var time4 = $('#time4');
+var time5 = $('#time5');
 
 // function to activate the time clock on page start
 var timeInterval = setInterval(function() {
     var time = moment().format('MMMM Do YYYY, h:mm:ss a');
     currentDay.text(time);
 },1000);
-// betweenTime();
+timeCompare();
 
-
+// setting text content of each time row
 var nineAm = beforeTime.add(0, 'hour').format('LT');
 $('#nineAm').text(nineAm);
 
@@ -42,35 +48,71 @@ var fivePm = beforeTime.add(1, 'hour').format('LT');
 $('#fivePm').text(fivePm);
 
 
+console.log(fivePm)
+console.log(currentTime)
+function timeCompare() {
+    // 9am
+    if (nineAm === currentTime) {
+        time9.addClass('present');
+    } else if (nineAm < currentTime) {
+        time9.addClass('past');
+    } else time9.addClass('future');
+
+    // 10am
+    if (tenAm === currentTime) {
+        time10.addClass('present');
+    } else if (tenAm < currentTime) {
+        time10.addClass('past');
+    } else time10.addClass('future');
+
+    // 11am
+    if (elevenAm === currentTime) {
+        time11.addClass('present');
+    } else if (elevenAm < currentTime) {
+        time11.addClass('past');
+    } else time11.addClass('future');
+
+    // 12pm
+    if (noon === currentTime) {
+        time12.addClass('present');
+    } else if (noon < currentTime) {
+        time12.addClass('past');
+    } else time12.addClass('future');
+
+    // 1pm
+    if (onePm === currentTime) {
+        time1.addClass('present');
+    } else if (onePm < currentTime) {
+        time1.addClass('past');
+    } else time1.addClass('future');
+
+    // 2pm
+    if (twoPm === currentTime) {
+        time2.addClass('present');
+    } else if (twoPm < currentTime) {
+        time2.addClass('past');
+    } else time2.addClass('future');
+
+    // 3pm
+    if (threePm === currentTime) {
+        time3.addClass('present');
+    } else if (threePm < currentTime) {
+        time3.addClass('past');
+    } else time3.addClass('future');
+
+    // 4pm
+    if (fourPm === currentTime) {
+        time4.addClass('present');
+    } else if (fourPm < currentTime) {
+        time4.addClass('past');
+    } else time4.addClass('future');
+
+    // 5pm
+    if (fivePm === currentTime) {
+        time5.addClass('present');
+    } else if (fivePm < currentTime) {
+        time5.addClass('past');
+    } else time5.addClass('future');
+};
 
 
-
-
-
-
-
-
-// function betweenTime(){
-//     if ( nineAm === currentTime) {
-//     time9.addClass('present');
-//     } else if (nineAm < beforeTime) {
-//     time9.addClass('past');
-//     } else {
-//     time9.addClass('future');
-//     };
-
-
-//     beforeTime =  moment().startOf('day').add(13, 'hours').format('LT');
-//     $('timeEl2').text(onePm);
-//     var onePm = beforeTime;
-//     if ( onePm === currentTime) {
-//     time13.addClass('present');
-//     } else if (onePm < beforeTime) {
-//     time13.addClass('past');
-//     } else {
-//     time9.addClass('future');
-//     };
-
-
-// };
-// console.log(currentTime);
